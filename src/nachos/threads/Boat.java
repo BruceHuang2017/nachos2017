eaccf3965c1e56e8261918b7e4d00f473047bfbc
BruceHuang2017 <huangyoupeng93@gmail.com>
2017-03-18 10:33:25 -0500

    time to wake up and keep working on this project.

--- src/nachos/threads/Boat.java
+++ src/nachos/threads/Boat.java
@@ -1,22 +1,128 @@
 package nachos.threads;
 import nachos.ag.BoatGrader;
 
+import java.awt.*;
+import java.awt.event.MouseListener;
+import java.util.LinkedList;
+
 public class Boat
 {
     static BoatGrader bg;
+
+    private static void letBrucePlay( int adults, int children, BoatGrader b ){
+
+        Lock tickets = new Lock();
+
+        //assume hawaiians are all civilized citizens, they wait in line to embark.
+        LinkedList<KThread> childrenInOahu = new LinkedList<>();
+        LinkedList<KThread> childrenInMolokai = new LinkedList<>();
+        LinkedList<KThread> adultInOahu = new LinkedList<>();
+        LinkedList<KThread> adultInMolokai = new LinkedList<>();
+
+        Island Oahu = new Island(adults+children);
+        Island Molokai = new Island(0);
+
+        someBoat noahsArk = new someBoat(Oahu, Molokai);
+
+        for(int i=0; i<adults; i++){
+            Hawaiian hawa = new Adult();
+            KThread t = new KThread(hawa);
+            t.setName("Adult "+i);
+            t.fork();
+            adultInOahu.add(t);
+            System.out.println("Fork " + t.getName());
+        }
+
+        for(int i=0; i<children; i++){
+            Hawaiian hawa = new Children();
+            KThread t = new KThread(hawa);
+            t.setName("Children "+i);
+            t.fork();
+            childrenInOahu.add(t);
+            System.out.println("Fork " + t.getName());
+        }
+
+
+
+    }
+
+
+
+    private interface Hawaii{
+        int numberOfIslandKnown = 2;
+    }
+
+    private static class Island implements Hawaii{
+        int initialPopulation;
+        int currentPopulation;
+        public Island(){}
+        private Island(int initialPopulation){
+            this.initialPopulation = initialPopulation;
+            this.currentPopulation = initialPopulation;
+        }
+
+    }
+
+    private static class someBoat{
+        public someBoat(){
+        }
+        private someBoat(Island made, Island dst){
+            this.made = made;
+            this.dst = dst;
+            this.currentLocation = made;
+        }
+        Hawaiian passenger = null;
+        Hawaiian pilot = null;
+        int weightLimit = 2;
+        int currentWeight = 0;
+        Island made;
+        Island dst;
+        Island currentLocation;
+        boolean isEmpty(someBoat a){
+            return a.currentWeight == 0;
+        }
+        boolean canEmbark(someBoat a){
+            return a.currentWeight != weightLimit;
+        }
+        boolean isFull(someBoat a){
+            return a.currentWeight == weightLimit;
+        }
+
+    }
+
+
+    public static abstract class Hawaiian implements Runnable{
+
+        public void run(){
+
+        }
+
+        void debug(){
+
+        }
+
+    }
+
+    public static class Adult extends Hawaiian{
+        public int weight = 2;
+
+    }
+
+    public static class Children extends Hawaiian{
+        public int weight = 1;
+
+    }
+
+
 
     public static void selfTest() {
         BoatGrader b = new BoatGrader();
 
         System.out.println("\n ***Testing Boats with only 2 children***");
-        begin(0, 2, b);
+        begin(789, 3, b);
 
-//	System.out.println("\n ***Testing Boats with 2 children, 1 adult***");
-//  	begin(1, 2, b);
-
-//  	System.out.println("\n ***Testing Boats with 3 children, 3 adults***");
-//  	begin(3, 3, b);
     }
+
 
     public static void begin( int adults, int children, BoatGrader b ) {
         // Store the externally generated autograder in a class
@@ -36,6 +142,8 @@ public class Boat
         KThread t = new KThread(r);
         t.setName("Sample Boat Thread");
         t.fork();
+
+        letBrucePlay(adults, children, b);
 
     }
 
