6461b1052df906650ad94d0c9769fcc2224d3dc0
BruceHuang2017 <huangyoupeng93@gmail.com>
2017-03-20 19:28:29 -0500

    before sunset

--- src/nachos/threads/Boat.java
+++ src/nachos/threads/Boat.java
@@ -1,9 +1,5 @@
 package nachos.threads;
 import nachos.ag.BoatGrader;
-import org.omg.PortableInterceptor.LOCATION_FORWARD;
-
-import java.util.LinkedList;
-import java.util.stream.StreamSupport;
 
 public class Boat
 {
@@ -20,26 +16,23 @@ public class Boat
     private Condition childrenOnMolokai;
     private Condition adultsOnOahu;
     private Condition adultsOnMolokai;
-    private Condition hawaiianOnBoat;
+    private Condition passengerOnBoat;
     
     public void letBrucePlay( int adults, int child, BoatGrader b ){
         isPlay = new Lock();
 
-        //assume hawaiians are all civilized citizens, they wait in line to embark.
-        // childrenOnOahu = new LinkedList<>();
-        // childrenOnMolokai = new LinkedList<>();
-        // adultsOnOahu = new LinkedList<>();
-        // adultsOnMolokai = new LinkedList<>();
-        childrenOnOahu = new Condition(isPlay);
-        childrenOnMolokai = new Condition(isPlay);
-        adultsOnOahu = new Condition(isPlay);
-        adultsOnMolokai = new Condition(isPlay);
-        hawaiianOnBoat = new Condition(isPlay);
+        childrenOnOahu = new Condition(isPlay); // will eventually leave island
+        childrenOnMolokai = new Condition(isPlay); // might need to back
+        adultsOnOahu = new Condition(isPlay);  // will be considered firstly
+        adultsOnMolokai = new Condition(isPlay); // finish, never need lock
+        passengerOnBoat = new Condition(isPlay);
 
-        Oahu = new Island(adults+child);
-        Molokai = new Island(0);
+        Oahu = new Island(adults, child);
+        Oahu.currentAdults = adults;
+        Oahu.currentChildern = child;
+        Molokai = new Island(0, 0);
 
-        someBoat noahsArk = new someBoat(born, destination);
+        someBoat noahsArk = new someBoat(born, destination, 2);
 
         for(int i=0; i<adults; i++){
             Hawaiian hawa = new Adult(born);
@@ -68,21 +61,27 @@ public class Boat
 
     private class Island implements Hawaii{
         int initialPopulation;
-        int currentPopulation;
+        int currentChildern;
+        int currentAdults;
         public Island(){}
-        private Island(int initialPopulation){
-            this.initialPopulation = initialPopulation;
-            this.currentPopulation = initialPopulation;
+        private Island(int adults, int child){
+            currentChildern = child;
+            currentAdults = adults;
+            this.initialPopulation = adults + child;
+        }
+        private int getCurrentPopulation(){
+            return currentAdults + currentChildern;
         }
     }
 
     public class someBoat{
         public someBoat(){
         }
-        private someBoat(Island made, Island dst){
+        private someBoat(Island made, Island dst, int weightLimit){
             this.made = made;
             this.dst = dst;
             this.currentLocation = made;
+            this.weightLimit = weightLimit;
         }
         Hawaiian passenger = null;
         Hawaiian pilot = null;
@@ -91,42 +90,109 @@ public class Boat
         Island made;
         Island dst;
         Island currentLocation;
-        boolean isEmpty(someBoat a){
-            return a.currentWeight == 0;
+        boolean isEmpty(){
+            return noahsArk.currentWeight == 0;
         }
-        boolean canEmbark(someBoat a){
-            return a.currentWeight != weightLimit;
+        boolean canEmbark(){
+            return noahsArk.currentWeight != weightLimit;
         }
-        boolean isFull(someBoat a){
-            return a.currentWeight == weightLimit;
+        boolean isFull(){
+            return noahsArk.currentWeight == weightLimit;
         }
-        boolean isOnBoat(Hawaiian a){
-            return a == passenger || a == pilot;
+        boolean isOnBoat(Hawaiian person){
+            return person == passenger || person == pilot;
         }
-        boolean isPilot(Hawaiian a){
-            return a == pilot;
+        boolean isPilot(Hawaiian person){
+            return person == pilot;
         }
-        boolean isPassenger(Hawaiian a){
-            return a == passenger;
+        boolean isPassenger(Hawaiian person){
+            return person == passenger;
+        }
+        void clearBoat(){
+            currentWeight = 0;
+            passenger = null;
+            pilot = null;
+        }
+
+        void depart(Island from, Island to){
+            if (from == to) return; // should never happen
+            isPlay.acquire();
+            if (from == this.currentLocation && !isEmpty()){
+                this.currentLocation = to;
+                if(pilot instanceof Adult){
+                    // must from O to M
+                    // always wake up and off the boat, sleep on M
+                    Oahu.currentAdults --;
+                    Molokai.currentAdults ++;
+                    ((Adult) pilot).setCurrentLocation(to);
+                    adultsOnMolokai.sleep();
+                    clearBoat();
+                }else if (passenger != null){
+                    // must from O to M
+                    // passenger sleep on boat, and pilot is current thread.
+                    Oahu.currentChildern = Oahu.currentChildern - 2;
+                    Molokai.currentChildern = Molokai.currentChildern + 2;
+                    pilot.currentLocation = to;
+                    childrenOnMolokai.sleep();
+                    passengerOnBoat.wake();
+                    passenger.currentLocation = to;
+                    childrenOnMolokai.sleep();
+                    clearBoat();
+                }else{
+                    // must from M to O, 1 pilot 0 passenger, child
+                    if (from != Molokai) return; // should never happen
+                    Molokai.currentChildern --;
+                    Oahu.currentChildern ++;
+                    pilot.currentLocation = to;
+                    childrenOnOahu.sleep();
+                    clearBoat();
+                }
+            }
+            isPlay.release();
+        }
+
+        void onBoard(Hawaiian person){
+            if(person instanceof Adult){
+                // has to from O to M, will wake up all the time.
+                if (currentLocation != Oahu) currentLocation = Oahu;
+                this.pilot = person;
+                this.passenger = person;
+                currentWeight += ((Adult) person).weight; // 2
+
+            }else if(passenger == null) {
+                this.passenger = person;
+                passengerOnBoat.sleep();
+                currentWeight += ((Child) person).weight; // 1
+
+            }else if(pilot == null){
+                this.pilot = person;
+                currentWeight += ((Child) person).weight; // 2
+            }
+        }
+
+        void ppSwitch(){
+            Hawaiian a = passenger;
+            passenger = pilot;
+            pilot = a;
         }
 
     }
 
     public abstract class Hawaiian implements Runnable{
 
-        Island born;
+        Island born = Oahu;
         Island currentLocation;
-        abstract
-
+        final Island targetLocation = Molokai;
+        abstract void bookTicket();
         public void run(){
             isPlay.acquire();
+            while (true){
+                if (Oahu.initialPopulation == Molokai.getCurrentPopulation()) return; // finished
+                if (this.currentLocation == noahsArk.currentLocation && noahsArk.canEmbark())
 
-            if(){
+                    bookTicket();
 
             }
-
-
-            isPlay.release();
         }
 
         void debug(String a){
@@ -138,11 +204,36 @@ public class Boat
     public class Adult extends Hawaiian{
         private Adult(Island born){
             this.born = born;
-            this.currentLocation = born;
+            this.setCurrentLocation(born);
         }
         public int weight = 2;
         Island born = null;
-        Island currentLocation = null;
+        private Island currentLocation = null;
+
+        @Override
+        void bookTicket(){
+            if(noahsArk.isEmpty()){
+                noahsArk.onBoard(this);
+                // bc i am pilot
+                isPlay.release();
+                noahsArk.depart(getCurrentLocation(), targetLocation);
+
+            }else{
+                // not able to embark
+                adultsOnOahu.sleep();
+                isPlay.release();
+            }
+
+        }
+
+
+        public Island getCurrentLocation() {
+            return currentLocation;
+        }
+
+        public void setCurrentLocation(Island currentLocation) {
+            this.currentLocation = currentLocation;
+        }
     }
 
     public class Child extends Hawaiian{
@@ -153,6 +244,32 @@ public class Boat
         public int weight = 1;
         Island born = null;
         Island currentLocation = null;
+
+        @Override
+        void bookTicket(){
+            if(currentLocation == Oahu){
+                // make sure two children on board
+                noahsArk.onBoard(this);
+                if(noahsArk.canEmbark() && noahsArk.isPassenger(this)) isPlay.release();
+                if(noahsArk.isFull() && noahsArk.isPilot(this)) {
+                    isPlay.release();
+                    noahsArk.depart(currentLocation, Molokai);
+                }
+
+            }else if(currentLocation == Molokai){
+                // only one child back to O
+                noahsArk.onBoard(this);
+                if(noahsArk.isPassenger(this) && noahsArk.canEmbark()){
+                    noahsArk.ppSwitch();
+                    isPlay.release();
+                    noahsArk.depart(currentLocation, Oahu);
+
+                }
+
+            }
+
+        }
+
     }
 
 
