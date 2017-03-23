7f413fa9ed68e32cdc7baf52b3be602740b25f76
BruceHuang2017 <huangyoupeng93@gmail.com>
2017-03-22 19:54:10 -0500

    another time, redo, understand lock queue, scheduler queue, and condition queue

--- src/nachos/threads/Boat.java
+++ src/nachos/threads/Boat.java
@@ -1,22 +1,33 @@
 package nachos.threads;
-import com.sun.deploy.util.SyncAccess;
 import nachos.ag.BoatGrader;
 
 public class Boat
 {
     static BoatGrader bg;
+    private static Lock boat = new Lock();
+    private static Condition adultsOnO = new Condition(boat);
+    private static Condition childrenOnO = new Condition(boat);
+    private static Condition adultsOnM = new Condition(boat);
+    private static Condition childrenOnM = new Condition(boat);
+    private static int numberOfAdultsOnO = 0;
+    private static int numberOfAdultsOnM = 0;
+    private static int numberOfChildrenOnO = 0;
+    private static int numberOfChildrenOnM = 0;
 
-    public static void selfTest() {
+
+
+
+    public static void selfTest(){
         BoatGrader b = new BoatGrader();
 
         System.out.println("\n ***Testing Boats with only 2 children***");
         begin(0, 2, b);
 
-//	System.out.println("\n ***Testing Boats with 2 children, 1 adult***");
-//  	begin(1, 2, b);
+        System.out.println("\n ***Testing Boats with 2 children, 1 adult***");
+        begin(1, 2, b);
 
-//  	System.out.println("\n ***Testing Boats with 3 children, 3 adults***");
-//  	begin(3, 3, b);
+        System.out.println("\n ***Testing Boats with 3 children, 3 adults***");
+        begin(3, 3, b);
     }
 
     public static void begin( int adults, int children, BoatGrader b ) {
@@ -25,66 +36,31 @@ public class Boat
         bg = b;
 
         // Instantiate global variables here
-        Lock boatLock = new Lock();
+        numberOfAdultsOnO = adults;
+        numberOfChildrenOnO = children;
 
-        island origin = new island();
-        origin.waitingList = new Condition(boatLock);
-        origin.adults = adults;
-        origin.child = children;
-
-        island dst = new island();
-        dst.waitingList = new Condition(boatLock);
-        dst.adults = 0;
-        dst.child = 0;
-
-        theboat aboat = new theboat();
-        aboat.passengers = new Condition(boatLock);
-        aboat.weight = 0;
-        aboat.loc = origin;
 
         // Create threads here. See section 3.4 of the Nachos for Java
         // Walkthrough linked from the projects page.
-        Runnable a = new Runnable() {
-            island myloc = origin;
-            @Override
-            public void run() {
-                boatLock.acquire();
-                while (true)
-                {
-                    if (dst.child>0 && aboat.weight==0 && aboat.loc == origin)
-                    {
-                        AdultItinerary(aboat,origin,dst,boatLock,myloc);
 
-                    }
+        for(int i=0; i<adults; i++){
+            KThread t = new KThread(new Runnable() {
+                @Override
+                public void run() {
+                    AdultItinerary();
                 }
-            }
-        };
-
-        Runnable c = new Runnable() {
-            island myloc = origin;
-            @Override
-            public void run() {
-                boatLock.acquire();
-                while (true)
-                {
-                    if (aboat.weight<2 && myloc == aboat.loc)
-                        ChildItinerary(aboat,origin,dst,boatLock,myloc);
-                }
-            }
-        };
-
-        for (int i=0;i<adults; i++){
-            KThread t = new KThread(a);
-            t.setName("Adult " + i);
+            });
             t.fork();
-            System.out.println("Fork " + t.getName());
         }
 
-        for (int i=0;i<children; i++){
-            KThread t = new KThread(c);
-            t.setName("Child " + i);
+        for(int i=0; i<children; i++){
+            KThread t = new KThread(new Runnable() {
+                @Override
+                public void run() {
+                    ChildItinerary();
+                }
+            });
             t.fork();
-            System.out.println("Fork " + t.getName());
         }
 
         Runnable r = new Runnable() {
@@ -96,122 +72,26 @@ public class Boat
         t.setName("Sample Boat Thread");
         t.fork();
 
+
+
     }
 
-    static class theboat {
-        Condition passengers;
-        int weight;
-        island loc;
-        void clear(){
-            weight=0;
-            passengers.wakeAll();
-            System.out.println("BoatClear");
-        }
-        void gameover(){
-            clear();
-            weight=2;
-            System.out.println("GameOver.");
-        }
-    }
-
-    static class island {
-        Condition waitingList;
-        int adults;
-        int child;
-    }
-
-    static void AdultItinerary(theboat aboat, island dst, island origin, Lock boatLock, island myloc) {
+    static void AdultItinerary() {
 	/* This is where you should put your solutions. Make calls
 	   to the BoatGrader to show that it is synchronized. For
 	   example:
 	       bg.AdultRowToMolokai();
 	   indicates that an adult has rowed the boat across to Molokai
 	*/
-        origin.adults--;
-        aboat.weight = 2;
-        aboat.passengers.sleep();
-        aboat.loc = dst;
-        aboat.clear();
-        myloc = dst;
-        bg.AdultRowToMolokai();
+	    boat.acquire();
 
-        dst.waitingList.wakeAll();
-        dst.adults++;
-        dst.waitingList.sleep();
 
-        boatLock.release(); // boat is ready
+
 
     }
 
-    static void ChildItinerary(theboat aboat, island dst, island origin, Lock boatLock, island myloc){
-        if (myloc == origin){
-            // origin logic
-            if(aboat.weight ==0){
-                // need one more
-                origin.child--;
-                aboat.weight=1;
-                aboat.passengers.sleep();
-                // this is the place when i wake up next time.
-                myloc=dst;
-                bg.ChildRideToMolokai();
-
-                dst.child++;
-                dst.waitingList.sleep();
-
-                boatLock.release();
-
-            }else if (aboat.weight == 1){
-                // embark and depart
-                origin.child--;
-                aboat.weight=2;
-                //aboat.passengers.sleep(); pilot cannot sleep
-                aboat.loc=dst;
-                aboat.clear();
-                myloc=dst;
-                bg.ChildRowToMolokai();
-
-                dst.waitingList.wakeAll();
-                dst.child++;
-                dst.waitingList.sleep();
-
-                boatLock.release();
-
-            }
-
-        }else if (myloc == dst){
-            // dst logic
-            if (origin.adults == 0 && origin.child == 0){
-                aboat.gameover();
-                // not release lock
-            }else if (aboat.weight == 0){
-                dst.child--;
-                aboat.weight=1;
-                // aboat.passengers.sleep(); pilot cannot sleep
-                aboat.loc=origin;
-                aboat.clear();
-                myloc=origin;
-                bg.ChildRowToOahu();
-
-                origin.waitingList.wakeAll();
-                origin.child++;
-                origin.waitingList.sleep();
-
-                boatLock.release();
-
-            }else if (aboat.weight == 1){
-                dst.waitingList.sleep();
-                boatLock.release();
-
-            }else {
-                System.out.println("unforeseen situation");
-            }
-
-        }else {
-            System.out.println("idk where i am.");
-            boatLock.release();
-        }
-
-
+    static void ChildItinerary()
+    {
     }
 
     static void SampleItinerary()
