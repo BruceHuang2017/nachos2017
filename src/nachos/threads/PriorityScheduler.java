b7a2b469ef4c9df8b81cdbce7c6e81809553a977
BruceHub <huangyoupeng93@gmail.com>
2017-03-25 08:55:36 -0500

    Update PriorityScheduler.java

--- src/nachos/threads/PriorityScheduler.java
+++ src/nachos/threads/PriorityScheduler.java
@@ -283,6 +283,7 @@ public class PriorityScheduler extends Scheduler {
 		 */
 		public void waitForAccess(PriorityQueue waitQueue) {
 			waitQueue.stateQueue.add(this);
+			if(waitQueue.transferPriority)
 			myPQList.add(waitQueue);
 
 		}
